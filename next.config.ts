5370f544cb2837345b0fd3ce4fc359e10b504c85
Fahad Abdul Qayyum <Fahad.45843@iqra.edu.pk>
2024-12-25 22:48:52 +0500

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
-// import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
-
-
-module.exports = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+const nextConfig: NextConfig = {
+  /* config options here */
 };
+
+export default nextConfig;
+
+
+
+// module.exports = {
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+// };
