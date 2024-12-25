1475cd02d836ddf7bb0181ee7200f4338e3081e1
Fahad Abdul Qayyum <Fahad.45843@iqra.edu.pk>
2024-12-26 01:21:11 +0500

    fakeapi created

--- src/pages/api/serverFetching/index.tsx
+++ src/pages/api/serverFetching/index.tsx
@@ -1,8 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'GET') {
-        res.status(200).json({ message: 'Hello from serverFetching API!' });
+        try {
+            const response = await fetch('https://fakestoreapi.com/products/1');
+            const data = await response.json();
+            return res.status(200).json({ data });
+        } catch (error) {
+            console.error('error...', error);
+        }
     } else if (req.method === 'POST') {
         const { data } = req.body;
         res.status(200).json({ receivedData: data });
