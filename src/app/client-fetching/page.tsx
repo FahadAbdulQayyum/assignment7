1a5a1936f762df7812e7777a567694727377880b
Fahad Abdul Qayyum <Fahad.45843@iqra.edu.pk>
2024-12-26 01:12:25 +0500

    code improved

--- src/app/client-fetching/page.tsx
+++ src/app/client-fetching/page.tsx
@@ -1,51 +1,35 @@
-import React from 'react'
+"use client"
 
-const page = () => {
+import React, { useEffect } from 'react'
+
+interface ClientFetchingType {
+    category: String,
+    description: String,
+    id: Number,
+    image: String,
+    price: Number,
+    rating: { rate: Number, count: Number },
+    title: String,
+}
+
+const ClientFetching = () => {
+    useEffect(() => {
+        const fetchFunction = async () => {
+            try {
+                const response = await fetch('https://fakestoreapi.com/products/1');
+                const data: ClientFetchingType = await response.json();
+                console.log('data...', data);
+            } catch (error) {
+                console.error('error...', error);
+            }
+        }
+        fetchFunction();
+    }, [])
     return (
         <div>
-            ppp...
+            {/* Render fetched data here */}
         </div>
     )
 }
 
-export default page
-
-
-
-
-
-// "use client"
-
-// import React, { useEffect } from 'react'
-
-// interface ClientFetchingType {
-//     category: String,
-//     description: String,
-//     id: Number,
-//     image: String,
-//     price: Number,
-//     rating: { rate: Number, count: Number },
-//     title: String,
-// }
-
-// const ClientFetching = () => {
-//     useEffect(() => {
-//         const fetchFunction = async () => {
-//             try {
-//                 const response = await fetch('https://fakestoreapi.com/products/1');
-//                 const data: ClientFetchingType = await response.json();
-//                 console.log('data...', data);
-//             } catch (error) {
-//                 console.error('error...', error);
-//             }
-//         }
-//         fetchFunction();
-//     }, [])
-//     return (
-//         <div>
-
-//         </div>
-//     )
-// }
-
-// export default ClientFetching
+export default ClientFetching
