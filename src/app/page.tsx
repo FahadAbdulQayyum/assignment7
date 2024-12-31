55e32f90b425dd103a7fb3529d994d61edc2cb6d
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2024-12-31 23:22:59 +0500

    Data fetched and presented the title and image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Image from 'next/image';
 import React, { useEffect, useState } from 'react'
 
 interface dataType {
@@ -14,7 +15,32 @@ interface dataType {
 
 const Page = () => {
 
-  const [APIData, setAPIData] = useState<dataType[]>([]);
+  const [APIData, setAPIData] = useState<dataType[]>([
+    // {
+    //   "id": 0,
+    //   "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    //   "price": 109.95,
+    //   "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    //   "category": "men's clothing",
+    //   "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    //   "rating": {
+    //     "rate": 3.9,
+    //     "count": 120
+    //   },
+    // },
+    // {
+    //   "id": 1,
+    //   "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    //   "price": 109.95,
+    //   "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    //   "category": "men's clothing",
+    //   "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    //   "rating": {
+    //     "rate": 3.9,
+    //     "count": 120
+    //   },
+    // }
+  ]);
 
   useEffect(() => {
     const fetchAPI = async () => {
@@ -33,8 +59,9 @@ const Page = () => {
         APIData.length
           ?
           <>
-            {APIData?.map((v, i) => <div key={i}>
+            {APIData?.map((v, i) => <div key={i} className="flex bg-gray-200 text-black my-2">
               <h1>{v.title}</h1>
+              <Image src={v.image} alt={v.title} width={50} height={50} />
             </div>)}
           </>
           :
